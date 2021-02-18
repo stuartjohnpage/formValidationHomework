1cd66c35d79289f37127e68af00ffdbf49203177
Stusigh <stuartjohnpage@live.com>
2021-02-18 07:16:07 -0600

    initial commit

---
+++ src/main/java/com/mywork/formvalidation/FormValidationSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mywork.formvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormValidationSpringProjectApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(FormValidationSpringProjectApplication.class, args);
+	}
+
+}
